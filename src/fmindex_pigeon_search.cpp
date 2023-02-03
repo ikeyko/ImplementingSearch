1ae0fa4f8c04dfe284b2187e017af276bb39c8dd
Katherina <lunches.global-0p@icloud.com>
2023-02-03 10:25:22 +0100

    add cuts

--- src/fmindex_pigeon_search.cpp
+++ src/fmindex_pigeon_search.cpp
@@ -19,6 +19,14 @@ int main(int argc, char const* const* argv) {
     auto query_file = std::filesystem::path{};
     parser.add_option(query_file, '\0', "query", "path to the query file");
 
+    size_t queries_size{};
+    size_t queries_size_default = 100;
+    parser.add_option(queries_size, 's', "queries-size", "Size of queries vector");
+
+    size_t errors_num{};
+    size_t errors_num_default = 0;
+    parser.add_option(errors_num, 'e', "errors", "Number of errors between 0 and 2");
+
     try {
          parser.parse();
     } catch (seqan3::argument_parser_error const& ext) {
@@ -35,6 +43,14 @@ int main(int argc, char const* const* argv) {
         queries.push_back(record.sequence());
     }
 
+    if ( ! (queries_size > 0 && queries_size <= 1000000 ) )  queries_size = queries_size_default;
+    if ( ! (errors_num >= 0 && errors_num <= 2 ) )  {
+        errors_num = errors_num_default;
+        seqan3::debug_stream << "This number of errors is not supported. Setting by defaul = 0\n";
+    }
+
+
+
     // loading fm-index into memory
     using Index = decltype(seqan3::fm_index{std::vector<std::vector<seqan3::dna5>>{}}); // Some hack
     Index index; // construct fm-index
@@ -48,8 +64,42 @@ int main(int argc, char const* const* argv) {
 
     seqan3::configuration const cfg = seqan3::search_cfg::max_error_total{seqan3::search_cfg::error_count{0}};
 
-    //!TODO here adjust the number of searches
-    queries.resize(100); // will reduce the amount of searches
+    // adjust queries vector size
+    std::vector<std::vector<seqan3::dna5>> queries_temp; 
+    if ( queries.size() > queries_size ) {
+        queries.resize(queries_size); 
+    } else {
+        queries_temp.insert(queries_temp.end(), queries.begin(), queries.end());
+        for (int i = 1; i<=10; ++i) {
+            queries.insert(queries.end(), queries_temp.begin(), queries_temp.end());
+            if ( queries.size() >= queries_size ) i = 99;
+        }
+        queries_temp.clear();
+    }
+
+    for (auto& query : queries) {
+        // save positions of query cuts
+        int *cuts = new int[errors_num + 2];
+        cuts[0] = 0;
+        int m = query.size();
+        int rest_len = m - m / (error_num + 1);
+        for (k = 1; k <= errors_num + 1; ++k) {
+            //first position
+            if (k == errors_num + 1) cuts[k] = m;
+            else {
+                int part_len = rest_len / (errors_num + 1 - k);
+                cuts[k] = cuts[k - 1] + part_len;
+                rest_len = rest_len - part_len;               
+            }        
+        }
+    for (auto& cut : cuts) {
+        std::cout<<cut<<" ";
+    }
+
+
+    }
+
+
 
 
     //!TODO !ImplementMe use the seqan3::search to find a partial error free hit, verify the rest inside the text
@@ -60,6 +110,8 @@ int main(int argc, char const* const* argv) {
     //          for (pos in search(index, part[p]):
     //              if (verify(ref, query, pos +- ....something)):
     //                  std::cout << "found something\n"
+
+
 
     return 0;
 }
