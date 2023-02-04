1bc12d551bbf2e8ba0d903fcf00618cc094af7ca
Katherina <lunches.global-0p@icloud.com>
2023-02-04 12:27:13 +0100

    debug

--- src/fmindex_pigeon_search.cpp
+++ src/fmindex_pigeon_search.cpp
@@ -115,7 +115,7 @@ int main(int argc, char const* const* argv) {
 
     // adjust queries vector size
     std::vector<std::vector<seqan3::dna5>> queries_temp; 
-    if ( queries.size() > queries_size ) {
+    if ( queries.size() >= queries_size ) {
         queries.resize(queries_size); 
     } else {
         queries_temp.insert(queries_temp.end(), queries.begin(), queries.end());
